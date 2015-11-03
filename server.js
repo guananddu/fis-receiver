998a634e40e0f5dc95ac34d217d81890348fc0b4
Pooky <guananddu@qq.com>
2015-11-03 14:13:41 +0800

    修改一处bug

--- server.js
+++ server.js
@@ -1,71 +1,77 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
+var http = require( 'http' );
+var formidable = require( 'formidable' );
+var fs = require( 'fs' );
+var path = require( 'path' );
+var mkdirp = require( 'mkdirp' );
 
-var server = http.createServer(function (req, res) {
-    
-    function error(err) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end(err.toString()); //fail
+var server = http.createServer( function( req, res ) {
+
+    function error( err ) {
+        res.writeHead( 500, {
+            'Content-Type': 'text/plain'
+        } );
+        res.end( err.toString() ); //fail
     }
 
-    function next(from, to) {
-        fs.readFile(from, function (err, content) {
-            if (err) {
-                error(err);
+    function next( from, to ) {
+        fs.readFile( from, function( err, content ) {
+            if ( err ) {
+                error( err );
             } else {
-                fs.writeFile(to, content, function (err) {
-                    if (err) {
-                        error(err);
+                fs.writeFile( to, content, function( err ) {
+                    if ( err ) {
+                        error( err );
                     }
-                    fs.unlink(from, function (err) {
-                        if (err) {
-                            error(err);
+                    fs.unlink( from, function( err ) {
+                        if ( err ) {
+                            error( err );
                         }
-                    });
-                    res.writeHead(200, {'Content-Type': 'text/plain'});
-                    res.end('0'); //success
-                });
+                    } );
+                    res.writeHead( 200, {
+                        'Content-Type': 'text/plain'
+                    } );
+                    res.end( '0' ); //success
+                } );
             }
-        });
+        } );
     }
 
-    if (req.url == '/') {
+    if ( req.url == '/' ) {
         // show a file upload form
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end('I\'m ready for that, you know.');
-    } else if (req.url == '/receiver' && req.method.toLowerCase() == 'post') {
+        res.writeHead( 200, {
+            'content-type': 'text/html'
+        } );
+        res.end( 'I\'m ready for that, you know.' );
+    } else if ( req.url == '/receiver' && req.method.toLowerCase() == 'post' ) {
         var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            if (err) {
-                error(err);
+        form.parse( req, function( err, fields, files ) {
+            if ( err ) {
+                error( err );
             } else {
-                var to = fields['to'];
-                fs.exists(to, function (exists) {
-                    if (exists) {
-                        fs.unlink(to, function (err) {
-                            next(files.file.path, to); 
-                        });
+                var to = fields[ 'to' ];
+                fs.exists( to, function( exists ) {
+                    if ( exists ) {
+                        fs.unlink( to, function( err ) {
+                            next( files.file && files.file.path || files[ 'null' ].path, to );
+                        } );
                     } else {
-                        fs.exists(path.dirname(to), function (exists) {
-                            if (exists) {
-                                next(files.file.path, to); 
+                        fs.exists( path.dirname( to ), function( exists ) {
+                            if ( exists ) {
+                                next( files.file && files.file.path || files[ 'null' ].path, to );
                             } else {
-                                mkdirp(path.dirname(to), 0777, function (err) {
-                                    if (err) {
-                                        error(err);
+                                mkdirp( path.dirname( to ), 0777, function( err ) {
+                                    if ( err ) {
+                                        error( err );
                                         return;
                                     }
-                                    next(files.file.path, to); 
-                                });
+                                    next( files.file && files.file.path || files[ 'null' ].path, to );
+                                } );
                             }
-                        });
+                        } );
                     }
-                });
+                } );
             }
-        });
+        } );
     }
-});
+} );
 module.exports = server;
